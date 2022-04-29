ea0a40858d75a000730d2cdb4f41fa1f1c896273
Andi Bade <andi.bade@gmail.com>
2022-04-29 07:44:27 +0200

    created cat command

--- src/create-jsonc.js
+++ src/create-jsonc.js
@@ -1,13 +1,12 @@
 import fs from "fs";
-import { basename, extname } from 'path';
+import { basename, extname } from "path";
 import { parse, stringify } from "comment-json";
 import debug from "debug";
 
-const debugLogger = debug('jomment')
+const debugLogger = debug("jomment");
 const SYM_PREFIX = "###SYM###-";
 
 function createSymbols(jsonWithEncodedSymbols, packageJson) {
-  console.log("packageJson", packageJson);
   for (const key in jsonWithEncodedSymbols) {
     if (key.startsWith(SYM_PREFIX)) {
       debugLogger("found symbol: ", key);
@@ -31,25 +30,16 @@ function createSymbols(jsonWithEncodedSymbols, packageJson) {
   }
 }
 
-export default function(jsonFilePath, jomFilePath) {
+export default function (jsonFilePath, jomFilePath) {
   const jomComments = JSON.parse(
     // skip first 5 lines (preamble)
-    fs.readFileSync(jomFilePath, "utf-8").split('\n').slice(5).join('\n')
+    fs.readFileSync(jomFilePath, "utf-8").split("\n").slice(5).join("\n")
   );
-  
+
   const jsonWithoutComments = parse(fs.readFileSync(jsonFilePath, "utf-8"));
   const jsonWithComments = jsonWithoutComments;
-  
-  createSymbols(
-    jomComments,
-    jsonWithComments
-  );
-  
-  const jsonBasename = basename(jsonFilePath, extname(jsonFilePath));
-  const jsoncFileName = `${jsonBasename}.jom.tmp.jsonc`;
 
-  fs.writeFileSync(jsoncFileName, stringify(jsonWithComments, null, 2));
-  
-  return jsoncFileName;
+  createSymbols(jomComments, jsonWithComments);
+
+  return stringify(jsonWithComments, null, 2);
 }
-
