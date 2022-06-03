525035be982946f8f40f7370b3910da705af62ea
Andi Bade <andi.bade@gmail.com>
2022-06-03 19:24:37 +0200

    no need for init command anymore

--- jom-cli.js
+++ jom-cli.js
@@ -23,33 +23,6 @@ program
   .version(selfInfo.version);
 
 program
-  .command("init [file]")
-  .description(`init an empty jom file for a json file`)
-  .option("-f, --force", "create file even if already exists", false)
-  .action((file, options) => {
-    file = file || DEFAULT_FILE;
-    const jsonBasename = basename(file, extname(file));
-    const jomFileName = `${jsonBasename}.jom`;
-    if (existsSync(jomFileName) && !options.force) {
-      console.error(chalk.red(`Error: file '${jomFileName}' already exists`));
-      console.error(
-        `if you want to recreate it with loosing all current comments, use --force`
-      );
-      process.exitCode = -23;
-      return;
-    }
-    console.log(
-      `initializing a basic .jom file '${jomFileName}' which holds the comments for '${file}'`
-    );
-    console.log(`now you can run`);
-    console.log(`  $> jom edit ${jomFileName}`);
-    console.log(`to start adding comments`);
-
-    const preamble = `${generatePreamble(file)}{}`;
-    writeFileSync(jomFileName, preamble);
-  });
-
-program
   .command("edit [jsonFile]")
   .description("edit a json file with its corresponding comments file")
   .action(async (jsonFile) => {
@@ -88,7 +61,7 @@ program
     const jsonBasename = basename(jsonFile, extname(jsonFile));
     const jomFile = join(dirname(jsonFile), `${jsonBasename}.jom`);
     const jsonc = await createJsonc(jsonFile, jomFile);
-    
+
     console.log(jsonc);
   });
 
